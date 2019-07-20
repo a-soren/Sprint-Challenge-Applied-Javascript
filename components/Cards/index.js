d2b21c95e428f6abc1f70ffd55800d4fc9c77f0b
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-07-19 21:25:46 -0600

    appended child elements

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,5 +18,33 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
-http.get('https://lambda-times-backend.herokuapp.com/articles');
+function makeArticleCard(){
+
+    // create elements
+    const card=document.createElement('div');
+    card.classList.add('card');
+
+    const headline=document.createElement('div');
+    headline.classList.add('headline');
+    headline.textContent=articleHeadline;
+
+    const author=document.createElement('div');
+    author.classList.add('author');
+
+    const imgContainer=document.createElement('div');
+    imgContainer.classList.add('img-container');
+    imgContainer.createElement('img',src=imgURL);
+    
+    const articleSpan=document.createElement('span');
+    articleSpan.textContent='By ',authorsName;
+
+
+    // appending elements
+
+    card.appendChild(headline);
+    card.appendChild(author);
+    author.appendChild(imgContainer);
+    author.appendChild(articleSpan);
+
+}
 
