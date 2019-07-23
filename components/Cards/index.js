5acdc81bf1255e8d8177a3d293988774cf567721
Amber Sorensen <ambersorensen@ambers-mbp.lan>
2019-07-22 22:45:10 -0600

    working on grabbing each element to create cards

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -19,38 +19,58 @@
 // Create a card for each of the articles and add the card to the DOM.
 
 axios.get('https://lambda-times-backend.herokuapp.com/articles').then((axiosArticleData)=>{
-        console.log(axiosArticleData.data);
+        console.log('article data here',axiosArticleData.data);
         let cards=axiosArticleData.data.article;
         cards.forEach(articleName =>{
         makeArticleCard(articleName);
     });
 
 function makeArticleCard(article){
+    let aCard=document.querySelector('.cards-container');
+    let cardDiv=document.createElement('div');
+    cardDiv.classList.add('card');
+    cardDiv.innerText=article;
 
-    // create elements
-    const card=document.createElement('div');
-    card.classList.add('card');
-    card.innerText=article;
-    console.log('card is here',card);
-    const headline=document.createElement('div');
-    headline.classList.add('headline');
-    headline.textContent=articleHeadline;
-    console.log('headline is here',headline);
-    const author=document.createElement('div');
-    author.classList.add('author');
+    console.log('cardDIV here', cardDiv);
 
-    const imgContainer=document.createElement('div');
-    imgContainer.classList.add('img-container');
-    imgContainer.createElement('img',src=imgURL);
+    let aHeadline=document.querySelector('.cards-container');
+    let headlineDIV=document.createElement('div');
+    headlineDIV.classList.add('headline');
+    headlineDIV.innerText=article;
+
+    console.log('headline here', headlineDIV);
+
+    let Author=document.querySelector('.cards-container');
+    let authorDIV=document.createElement('div');
+    authorDIV.classList.add('author');
+    authorDIV.innerText=article;
+
+    console.log('Author here', authorDIV);
+
+    let imgContainer=document.querySelector('.cards-container');
+    let imgContA=document.createElement('div');
+    imgContA.classList.add('img-container');
+    imgContA.innerText=article;
+
+    console.log('img container here', imgContA);
+
+    let imgSrc=document.querySelector('.cards-container');
+    let imgSrcA=document.createElement('img');
+    imgSrcA.src.add(url);
+    imgSrcA.innerText=article;
+
+    console.log('img and src here', imgSrcA);
+
+    let aSpan=document.querySelector('.cards-container');
+    let spanElement=document.createElement('span');
+    spanElement.textContent='By `${Author}';
+    spanElement.innerText=article;
+
+    console.log('Span element here', spanElement);
     
-    const articleSpan=document.createElement('span');
-    articleSpan.textContent='By ',authorsName;
+    card.appendChild(headlineDIV);
+    card.appendChild(authorDIV);
+    Author.appendChild(imgContA);
+    Author.appendChild(spanElement);
 
-
-    // appending elements
-
-    card.appendChild(headline);
-    card.appendChild(author);
-    author.appendChild(imgContainer);
-    author.appendChild(articleSpan);
-};
+}
